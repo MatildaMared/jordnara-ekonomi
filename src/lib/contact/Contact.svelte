5ab631e8525ca7cf37fb5a71c21601dc8efe5d1f
Matilda Mared <matildamared@live.se>
2022-08-27 13:35:19 +0200

    add styling

--- src/lib/contact/Contact.svelte
+++ src/lib/contact/Contact.svelte
@@ -2,19 +2,27 @@
 	const API_KEY = "AIzaSyB32uPsHhFH6LILWZraOjY_irVKz_NyhOg";
 </script>
 
-<h1>Kontakt</h1>
+<section class="contact">
+	<h1>Kontakt</h1>
+	<iframe
+			class="map"
+			loading="lazy"
+			allowfullscreen
+			referrerpolicy="no-referrer-when-downgrade"
+			src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&zoom=10&maptype=satellite&q=Mossarp+406+313+93+Slättåkra`}>
+	</iframe>
+</section>
 
-<iframe
-		class="map"
-		loading="lazy"
-		allowfullscreen
-		referrerpolicy="no-referrer-when-downgrade"
-		src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&zoom=10&maptype=satellite&q=Mossarp+406+313+93+Slättåkra`}>
-</iframe>
 
 <style lang="scss">
-	.map {
-		border: none;
+	.contact {
 		width: 100%;
+		max-width: var(--max-width);
+	}
+
+	.map {
+		border: 1px solid hsla(131, 20%, 18%, .95);
+		width: 100%;
+		height: 300px;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,13 +17,9 @@
 
 <style lang="scss">
 	main {
-		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
