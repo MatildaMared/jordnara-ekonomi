fd22dccaa771d8954eb4bfa7b219c30527203d2d
Matilda Mared <matildamared@live.se>
2022-08-27 09:43:35 +0200

    update fonts and add contents to header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -2,13 +2,36 @@
 </script>
 
 <header>
-	<h1>Jordnära ekonomi</h1>
+	<h1 class="title">Jordnära ekonomi</h1>
+	<p class="subtitle">Redovisning med fötterna på jorden</p>
 </header>
 
-<style>
+<style lang="scss">
 	header {
-		background: #EEE url("/images/skog.jpg") no-repeat center;
+		background: linear-gradient(
+						rgba(0, 0, 0, 0.15),
+						rgba(0, 0, 0, 0.30)
+		), url("/images/skog.jpg") no-repeat center;
 		background-size: cover;
+		height: 100%;
 		min-height: 80vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: rgba(255, 255, 255, .9);
+		text-align: center;
+		line-height: 1.2;
+
+		.title {
+			font-size: 4.5rem;
+			text-shadow: 0 4px 8px hsla(131, 20%, 8%, .5);
+		}
+
+		.subtitle {
+			font-family: var(--font-script);
+			font-size: 2rem;
+			text-shadow: 0 3px 6px hsla(131, 20%, 8%, .3);
+		}
 	}
 </style>
