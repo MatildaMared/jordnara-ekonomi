22b91829f6f40a1c1bfc649e73225a19ebfd9201
Matilda Mared <matildamared@live.se>
2022-08-28 13:42:10 +0200

    add styling for contact section

--- src/lib/contact/Contact.svelte
+++ src/lib/contact/Contact.svelte
@@ -9,21 +9,27 @@
 	<div class="content_wrapper">
 		<article class="contact__wrapper">
 			<div class="contact__group">
-				<PhoneIcon/>
+				<div class="icon">
+					<PhoneIcon size="32"/>
+				</div>
 				<div>
 					<h3>Telefon</h3>
 					<a href="tel:070-3369170">070-3369170</a>
 				</div>
 			</div>
 			<div class="contact__group">
-				<MailIcon/>
+				<div class="icon">
+					<MailIcon size="32"/>
+				</div>
 				<div>
 					<h3>E-post</h3>
 					<a href="mailto:jordnaraekonomi@outlook.com">jordnaraekonomi@outlook.com</a>
 				</div>
 			</div>
 			<div class="contact__group">
-				<MapPinIcon/>
+				<div class="icon">
+					<MapPinIcon size="32"/>
+				</div>
 				<div>
 					<h3>Adress</h3>
 					<p>Mossarp 406, 313 93 Slättåkra</p>
@@ -58,26 +64,46 @@
 
 		&__wrapper {
 			background-color: rgba(255, 255, 255, .85);
-			width: 500px;
-			max-width: 100%;
+			width: 100%;
 			padding: 32px;
-			margin: var(--spacing-m) 0;
 			border-radius: 8px;
 			border: 1px solid hsla(131, 20%, 22%, .95);
+			height: 100%;
+			flex: 1;
 		}
 
 		&__group {
-			border: 1px solid red;
-			margin-bottom: var(--spacing-s);
+			margin-bottom: 48px;
 			display: flex;
 			align-items: center;
+			position: relative;
+
+			&::after {
+				opacity: .25;
+				content: "";
+				width: 75%;
+				height: 1px;
+				background-color: var(--color-primary-dark);
+				position: absolute;
+				top: calc(100% + 24px);
+				left: 50%;
+				transform: translateX(-50%);
+			}
 
 			&:last-of-type {
 				margin-bottom: 0;
+
+				&::after {
+					display: none;
+				}
 			}
 
 			& > div {
 				margin-left: 16px;
+			}
+
+			h3, .icon {
+				color: var(--color-primary-dark);
 			}
 		}
 	}
@@ -85,14 +111,14 @@
 	.content_wrapper {
 		width: 100%;
 		display: flex;
-		align-items: center;
 		gap: 32px;
 	}
 
 	.map {
+		flex: 2;
 		border: 1px solid hsla(131, 20%, 22%, .95);
 		width: 100%;
-		height: 300px;
+		max-height: 400px;
 		border-radius: 8px;
 	}
 </style>
